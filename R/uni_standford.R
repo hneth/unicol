45777e9806f4009cd4a6822a71c3c284ff37d7b6
Hansjörg Neth <hansneth@gmail.com>
2023-06-14 11:19:11 +0200

    bug fix (quotes in documentation)

--- R/uni_standford.R
+++ R/uni_standford.R
@@ -74,8 +74,12 @@
 #'
 #' @export
 
-uni_stanford_primary_1 <- unikn::newpal(col = c("#8C1515", "#B83A4B", "#820000", "#FFFFFF", "#2E2D29", "#43423E", "#585754", "#6D6C69", "#767674", "#979694", "#ABABA9", "#C0C0BF", "#D5D5D4", "#EAEAEA", "#53565A"),
-                           names = c("cardinal_red", "cardinal_red_light", "cardinal_red_dark", "white", "black", "black_90", "black_80", "black_70", "black_60", "black_50", "black_40", "black_30", "black_20", "black_10", "cool_grey"),
+uni_stanford_primary_1 <- unikn::newpal(col = c("#8C1515", "#B83A4B", "#820000", 
+                                                "#FFFFFF", 
+                                                "#2E2D29", "#43423E", "#585754", "#6D6C69", "#767674", "#979694", "#ABABA9", "#C0C0BF", "#D5D5D4", "#EAEAEA", "#53565A"),
+                           names = c("cardinal_red", "cardinal_red_light", "cardinal_red_dark", 
+                                     "white", 
+                                     "black", "black_90", "black_80", "black_70", "black_60", "black_50", "black_40", "black_30", "black_20", "black_10", "cool_grey"),
                            as_df = FALSE)
 
 # # Check:
@@ -93,7 +97,7 @@ uni_stanford_primary_1 <- unikn::newpal(col = c("#8C1515", "#B83A4B", "#820000",
 #' Although our primary palette should drive most Stanford communications materials, other colors can be added as appropriate.
 #' For those circumstances, we have developed this accent palette.
 #'
-#' These colors are intended to compliment our Stanford primary colors:
+#' These colors are intended to compliment our Stanford primary colors: 
 #' \code{"palo_alto"} (defined as HEX #175E54),
 #' \code{"palo_alto_light"} (defined as HEX #2D716F),
 #' \code{"palo_alto_dark"} (defined as HEX #014240),
@@ -135,7 +139,7 @@ uni_stanford_primary_1 <- unikn::newpal(col = c("#8C1515", "#B83A4B", "#820000",
 #' \code{"stone_dark"} (defined as HEX #544948),
 #' \code{"fog"} (defined as HEX #DAD7CB),
 #' \code{"fog_light"} (defined as HEX #F4F4F4) and
-#' \code{"fog_dark""} (defined as HEX #B6B1A9")
+#' \code{"fog_dark"} (defined as HEX #B6B1A9)
 #'
 #' @return
 #' A named vector of colors (HEX/HTML codes of type character).
@@ -164,8 +168,20 @@ uni_stanford_primary_1 <- unikn::newpal(col = c("#8C1515", "#B83A4B", "#820000",
 #'
 #' @export
 
-uni_stanford_accent_2 <- unikn::newpal(col = c("#175E54", "#2D716F", "#014240", "#279989", "#59B3A9", "#017E7C", "#8F993E", "#A6B168", "#7A863B", "#6FA287", "#8AB8A7", "#417865", "#4298B5", "#67AFD2", "#016895", "#007C92", "#009AB4", "#006B81", "#E98300", "#F9A44A", "#D1660F", "#E04F39", "#F4795B", "#C74632", "#FEDD5C", "#FFE781", "#FEC51D", "#620059", "#734675", "#350D36", "#651C32", "#7F2D48", "#42081B", "#5D4B3C", "#766253", "#2F2424", "#7F7776", "#D4D1D1", "#544948", "#DAD7CB", "#F4F4F4", "#B6B1A9"),
-                           names = c("palo_alto", "palo_alto_light", "palo_alto_dark", "palo_verde", "palo_verde_light", "palo_verde_dark", "olive", "olive_light", "olive_dark", "bay", "bay_light", "bay_dark", "sky", "sky_light", "sky_dark", "lagunita", "lagunita_light", "lagunita_dark", "poppy", "poppy_light", "poppy_dark", "spirited", "spirited_light", "spirited_dark", "illuminating", "illuminating_light", "illuminating_dark", "plum", "plum_light", "plum_dark", "brick", "brick_light", "brick_dark", "archway", "archway_light", "archway_dark", "stone", "stone_light", "stone_dark", "fog", "fog_light", "fog_dark"),
+uni_stanford_accent_2 <- unikn::newpal(col = c("#175E54", "#2D716F", "#014240", "#279989", "#59B3A9", "#017E7C", 
+                                               "#8F993E", "#A6B168", "#7A863B", "#6FA287", "#8AB8A7", "#417865", 
+                                               "#4298B5", "#67AFD2", "#016895", "#007C92", "#009AB4", "#006B81", 
+                                               "#E98300", "#F9A44A", "#D1660F", "#E04F39", "#F4795B", "#C74632", 
+                                               "#FEDD5C", "#FFE781", "#FEC51D", "#620059", "#734675", "#350D36", 
+                                               "#651C32", "#7F2D48", "#42081B", "#5D4B3C", "#766253", "#2F2424", 
+                                               "#7F7776", "#D4D1D1", "#544948", "#DAD7CB", "#F4F4F4", "#B6B1A9"),
+                           names = c("palo_alto", "palo_alto_light", "palo_alto_dark", "palo_verde", "palo_verde_light", "palo_verde_dark", 
+                                     "olive", "olive_light", "olive_dark", "bay", "bay_light", "bay_dark", 
+                                     "sky", "sky_light", "sky_dark", "lagunita", "lagunita_light", "lagunita_dark", 
+                                     "poppy", "poppy_light", "poppy_dark", "spirited", "spirited_light", "spirited_dark", 
+                                     "illuminating", "illuminating_light", "illuminating_dark", "plum", "plum_light", "plum_dark", 
+                                     "brick", "brick_light", "brick_dark", "archway", "archway_light", "archway_dark", 
+                                     "stone", "stone_light", "stone_dark", "fog", "fog_light", "fog_dark"),
                            as_df = FALSE)
 
 # # Check:
@@ -223,8 +239,12 @@ uni_stanford_accent_2 <- unikn::newpal(col = c("#175E54", "#2D716F", "#014240", 
 #'
 #' @export
 
-uni_stanford_web_3 <- unikn::newpal(col = c("#B1040E", "#E50808", "#820000", "#006CB8", "#6FC3FF", "#00548f", "#008566", "#1AECBA", "#006F54"),
-                           names = c("digital_red", "digital_red_light", "digital_red_dark", "digital_blue", "digital_blue_light", "digital_blue_dark", "digital_green", "digital_green_light", "digital_green_dark"),
+uni_stanford_web_3 <- unikn::newpal(col = c("#B1040E", "#E50808", "#820000", 
+                                            "#006CB8", "#6FC3FF", "#00548f", 
+                                            "#008566", "#1AECBA", "#006F54"),
+                           names = c("digital_red", "digital_red_light", "digital_red_dark", 
+                                     "digital_blue", "digital_blue_light", "digital_blue_dark", 
+                                     "digital_green", "digital_green_light", "digital_green_dark"),
                            as_df = FALSE)
 
 # # Check:
