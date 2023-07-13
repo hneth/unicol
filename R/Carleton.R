afa89d4a1f19982b05843b5d9812a7c2ba8973f4
Hansjörg Neth <hansneth@gmail.com>
2023-07-13 07:27:36 +0200

    bug fix: link to source

--- R/Carleton.R
+++ R/Carleton.R
@@ -1,8 +1,8 @@
-## Carleton.R | 2023 07 12
+## Carleton.R | 2023 07 13
 ## Colors of the University of Carleton, Canada 
-## -----------------------------------------------
+## --------------------------------------------
 
-# Information: -----------------------------------
+# Information: ------ 
 
 # uni_pals: "carleton_1" (1)
 # inst: "University of Carleton" (in English)
@@ -27,12 +27,10 @@
 #' of the \href{https://carleton.ca/}{University of Carleton}, Canada.
 #'
 #' The two official colors are
-#' \code{"maize"} (defined as RGB 255/223/79, HEX #FFDF4F), and
-#' \code{"blue_carleton"} (defined as RGB 0/48/105, HEX #003069).
-#'
+#' \code{"maize"} (defined as Pantone 122, RGB 255/223/79, or HEX #FFDF4F), and
+#' \code{"blue_carleton"} (defined as Pantone 294, RGB 0/48/105, or HEX #003069).
 #'
 #' \code{carleton_1} uses the HEX color definition. 
-#' 
 #' 
 #' @return 
 #' A named vector of colors (HEX/HTML codes of type character).
@@ -42,11 +40,12 @@
 #'
 #' @source 
 #' Color definitions are based on the
-#' \href{https://www.carleton.edu/communications/resources/college-wordmark-identity-graphics/college-colors/#:~:text=The%20College%27s%20official%20colors%20are%20maize%20and%20blue.}{Website of the University of Carleton}.
+#' \href{https://www.carleton.edu/communications/resources/college-wordmark-identity-graphics/college-colors/}{University of Carleton's website}.
 #'
 #' @examples
 #' carleton_1
-#' unikn::seecol(carleton_1, main = "University of Carleton") # view color palette
+#' unikn::seecol(carleton_1, 
+#'               main = "Colors of the University of Carleton") # view color palette
 #'
 #' @family university color palettes
 #'
