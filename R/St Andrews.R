558e845a724fc1feeb1c2b64fdaf094dc01f979a
hneth <hansneth@gmail.com>
2023-07-18 11:25:37 +0200

    bug fixes

--- R/St Andrews.R
+++ R/St Andrews.R
@@ -10,24 +10,23 @@
 # URL: "https://www.st-andrews.ac.uk/"
 
 # Color source:
-
 # URL: "https://www.st-andrews.ac.uk/brand/design-guidelines/colours/"
 
 
 
 # Color palette(s):
 
-#- pal_standrews_1: Primary Colors
+# - pal_standrews_1: Primary colors: ------ 
 
 #' Colors of the University of St Andrews
 #' 
-#' \code provides the 4 primary colors 
-#' of the \href{https://www.st-andrews.ac.uk/brand/design-guidelines/colours/}{University of St Andrews}, UK
+#' \code{pal_standrews_1} provides the 4 primary colors 
+#' of the \href{https://www.st-andrews.ac.uk/brand/design-guidelines/colours/}{University of St Andrews}, UK. 
 #' 
 #' The 4 primary colors are
-#' \code{“andrews_blue"}(defined as HEX #00539b)
+#' \code{“andrews_blue"}(defined as HEX #00539b), 
 #' \code{"andrews_red"}(defined as HEX #ee312a),
-#' \code {"andrews_yellow"}(defined as HEX #ffdf00),
+#' \code{"andrews_yellow"}(defined as HEX #ffdf00), and 
 #' \code{"andrews_black"}(defined as HEX #231f20).
 #' 
 #' \code{pal_standrews_1} uses the HEX color definition. 
@@ -39,8 +38,8 @@
 #' \strong{unicol}, 2023-07-16.
 #'
 #' @source 
-#' Color definitions are based on the
-#' \href{https://www.st-andrews.ac.uk/brand/design-guidelines/colours/}.
+#' Color definitions are based on 
+#' \href{https://www.st-andrews.ac.uk/brand/design-guidelines/colours/}{St. Andrews' design guidelines}.
 #'
 #' @examples
 #' pal_standrews_1
@@ -57,26 +56,28 @@
 #'
 #' @export
 
-pal_standrews_1 <-unikn::newpal(col = c("#00539b","#ee312a","#ffdf00","#231f20"), names = (c("andrews_blue","andrews_red","andrews_yellow","andrews_black"), as_df= FLASE))
+pal_standrews_1 <- unikn::newpal(col = c("#00539b","#ee312a","#ffdf00","#231f20"), 
+                                 names = c("andrews_blue","andrews_red","andrews_yellow","andrews_black"), 
+                                 as_df = FALSE)
 
-# - pal_standrews_2: Secondary colors
+
+
+# - pal_standrews_2: Secondary colors ------ 
 
 #' Colors of the University of St Andrews
 #' 
-#' \code provides the 8 secondary colors 
-#' of the \href{https://www.st-andrews.ac.uk/brand/design-guidelines/colours/}{University of St Andrews}, UK
+#' \code{pal_standrews_2} provides the 8 secondary colors 
+#' of the \href{https://www.st-andrews.ac.uk/brand/design-guidelines/colours/}{University of St Andrews}, UK. 
 #' 
 #' The 8 secondary colors are
-#' \code{“andrews_light_blue"}(defined as HEX #00aeef)
+#' \code{“andrews_light_blue"}(defined as HEX #00aeef), 
 #' \code{"andrews_mid_blue"}(defined as HEX #007dc5),
 #' \code {"andrews_burgundy"}(defined as HEX #c60c46),
 #' \code{"andrews_purple"}(defined as HEX #7b439a)
 #' \code {"andrews_orange"}(defined as HEX #f5842b),
-#' \code{"andrews_green"}(defined as HEX #54b948)
-#' \code{"andrews_mid_green"}(defined as HEX #00853f)
+#' \code{"andrews_green"}(defined as HEX #54b948), 
+#' \code{"andrews_mid_green"}(defined as HEX #00853f), and 
 #' \code{"andrews_dark_green"}(defined as HEX #005953).
-#' 
-#'
 #' 
 #' \code{pal_standrews_1} uses the HEX color definition. 
 #' 
@@ -87,8 +88,8 @@ pal_standrews_1 <-unikn::newpal(col = c("#00539b","#ee312a","#ffdf00","#231f20")
 #' \strong{unicol}, 2023-07-16.
 #'
 #' @source 
-#' Color definitions are based on the
-#' \href{https://www.st-andrews.ac.uk/brand/design-guidelines/colours/}.
+#' Color definitions are based on 
+#' \href{https://www.st-andrews.ac.uk/brand/design-guidelines/colours/}{St. Andrews' design guidelines}.
 #'
 #' @examples
 #' pal_standrews_2
@@ -105,12 +106,15 @@ pal_standrews_1 <-unikn::newpal(col = c("#00539b","#ee312a","#ffdf00","#231f20")
 #'
 #' @export
 
-pal_standrews_2 <- unikn::newpal(col = c("#00aeef","#007dc5","#c60c46","#7b439a","#f5842b","#54b948","#00853f","#005953"), names = c("andrews_light_blue","andrews_mid_blue","andrews_burgundy","andrews_purple","andrews_orange","andrews_green","andrews_mid_green","andrews_dark_green"), as_df = FALSE)
+pal_standrews_2 <- unikn::newpal(col = c("#00aeef", "#007dc5", "#c60c46", "#7b439a", 
+                                         "#f5842b", "#54b948", "#00853f", "#005953"), 
+                                 names = c("standrews_light_blue", "standrews_mid_blue", "standrews_burgundy", "standrews_purple", 
+                                           "standrews_orange", "standrews_green", "standrews_mid_green","standrews_dark_green"), 
+                                 as_df = FALSE)
 
-# # Check: 
-
-#unikn::seecol(pal_standrews_1, main = "Primary colors of the University of St Andrews", col_bg = "andrews_blue")
-#unikn::seecol(conc2, main = "Secondary colors of the University of St Andrews", col_bg = "andrews_orange")
+## Check: 
+# unikn::seecol(pal_standrews_1, main = "Primary colors of the University of St Andrews", col_bg = "andrews_blue")
+# unikn::seecol(conc2, main = "Secondary colors of the University of St Andrews", col_bg = "andrews_orange")
 
 
 
@@ -119,7 +123,6 @@ pal_standrews_2 <- unikn::newpal(col = c("#00aeef","#007dc5","#c60c46","#7b439a"
 # - etc.
 
 ## eof. ----------
-
 
 
 
