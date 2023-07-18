6d578b78ef872d28b609b7e86720b02fba5a464b
hneth <hansneth@gmail.com>
2023-07-18 12:19:24 +0200

    bug fixes: colors and palette names

--- R/Irvine.R
+++ R/Irvine.R
@@ -1,10 +1,10 @@
-## Irvine.R|2023 07 13
+## Irvine.R | 2023 07 13
 ## Colors of the University of California, Irvine, USA
-# ------------------------------------------
+## ----------------------------------------------------
 
 # Information: ----
 
-# unipals: "pal_uci_primary", "pal_uci_secondary", "pal_ucihealth_secondary", "pal_ucihealth_tertiary" (4)
+# unipals: "pal_uci_1", "pal_uci_secondary", "pal_ucihealth_2", "pal_ucihealth_3" (4)
 # inst: "University of California, Irvine" 
 # country: USA
 # URL: "https://www.uci.edu"
@@ -15,37 +15,38 @@
 
 # Color palettes:
 
-# - pal_uci_primary: Primary colors ----
 
-#'Colors of the University of California, Irvine:
+# - pal_uci_1: Primary colors ----
+
+#' Primary colors of the University of California, Irvine 
 #'
-#'\code{pal_uci_primary} provides the 2 primary colors
-#'of the \href{https://www.uci.edu}{University of California, Irvine}, USA.
+#' \code{pal_uci_1} provides the 2 primary colors
+#' of the \href{https://www.uci.edu}{University of California, Irvine}, USA.
 #'
-#'The 2 primary colors are
-#'\code{"uci_blue"}(defined as HEX #0064a4) and
-#'\code{"uci_gold"}(defines as HEX #ffd200).
+#' The 2 primary colors are
+#' \code{"uci_blue"}(defined as HEX #0064a4) and
+#' \code{"uci_gold"}(defines as HEX #ffd200).
 #'
-#'\code{pal_uci_primary} uses the HEX color definition.
+#' \code{pal_uci_1} uses the HEX color definition.
 #'
-#'@return
+#' @return
 #' A names vector of colors (HEX/HTML codes of type character).
 #' 
-#'@author
-#'\strong{unicol}, 2023-07-07.
+#' @author
+#' \strong{unicol}, 2023-07-07.
 #'
-#'@source
-#'Color definitions are based on the
-#'\href{https://brand.uci.edu/master-branding/color-palette/}.
+#' @source
+#' Color definitions are based on 
+#' \href{https://brand.uci.edu/master-branding/color-palette/}{UCI's brand guide}.
 #'
-#'@examples
-#'pal_uci_primary
-#'unikn::seecol(pal_uci_primary, main= "University of California, Irvine") #view color palette.
+#' @examples
+#' pal_uci_1
+#' unikn::seecol(pal_uci_1, main= "University of California, Irvine") #view color palette.
 #'
-#'@family university color palettes
+#' @family university color palettes
 #'
-#'@seealso
-#'\code{\link{seecol}} for viewing and comparing color palettes;
+#' @seealso
+#' \code{\link{seecol}} for viewing and comparing color palettes;
 #' \code{\link{usecol}} for using color palettes;
 #' \code{\link{simcol}} for finding similar colors;
 #' \code{\link{newpal}} for defining new color palettes;
@@ -53,12 +54,17 @@
 #' 
 #' @export
 
-pal_uci_primary <- unikn::newpal(col = c("#0064a4", "#ffd200"), names = c("uci_blue","uci_gold"), as_df = FALSE)
+pal_uci_1 <- unikn::newpal(col = c("#0064a4", "#ffd200"), 
+                           names = c("uci_blue","uci_gold"), 
+                           as_df = FALSE)
+
+
 
 
 # - pal_uci_secondary: Secondary colors -----
 
-#' Colors of the University of California, Irvine
+
+#' Secondary colors of the University of California, Irvine
 #' 
 #' \code{pal_uci_secondary} provides the 7 secondary colors
 #' of the \href{https://www.uci.edu}{University of California, Irvine}, USA.
@@ -67,10 +73,10 @@ pal_uci_primary <- unikn::newpal(col = c("#0064a4", "#ffd200"), names = c("uci_b
 #' \code{"light_blue"}(defined as HEX #6aa2b8),
 #' \code{"light_gray"}(defined as HEX #c6beb5),
 #' \code{"dark_blue"}(defined as HEX #1b3d6d),
-#' \code{"orange"} (defined as HEX #f78d2d)
-#' \code{"light_yellow"} (defined as HEX #f7eb5f)
-#' \code{"dark_gray"} (defined as HEX #555759)
-#' \code{"lime_green"} (defined as HEX #7ab800)
+#' \code{"orange"} (defined as HEX #f78d2d), 
+#' \code{"light_yellow"} (defined as HEX #f7eb5f), 
+#' \code{"dark_gray"} (defined as HEX #555759), and 
+#' \code{"lime_green"} (defined as HEX #7ab800). 
 #' 
 #' \code{pal_uci_secondary} uses the HEX color definition.
 #' 
@@ -81,8 +87,8 @@ pal_uci_primary <- unikn::newpal(col = c("#0064a4", "#ffd200"), names = c("uci_b
 #' \strong{unicol}, 2320-07-13.
 #' 
 #' @source 
-#' Color definitions are based on the
-#' \href{https://brand.uci.edu/master-branding/color-palette/}
+#' Color definitions are based on 
+#' \href{https://brand.uci.edu/master-branding/color-palette/}{UCI's brand guide}.
 #'
 #' @examples 
 #' pal_uci_secondary
@@ -98,24 +104,30 @@ pal_uci_primary <- unikn::newpal(col = c("#0064a4", "#ffd200"), names = c("uci_b
 #' \code{\link{grepal}} for finding named colors.
 #' 
 #' @export
-pal_uci_secondary <- unikn::newpal(col = c("#6aa2b8","#c6beb5","#1b3d6d","#f78d2d","#f7eb5f", "#555759", "7ab800"), names = c("light_blue","light_gray", "dark_blue","orange","light_yellow", "dark_gray", "lime_green"), as_df = FALSE)
+
+pal_uci_secondary <- unikn::newpal(col = c("#6aa2b8", "#c6beb5", "#1b3d6d", "#f78d2d", 
+                                           "#f7eb5f", "#555759", "#7ab800"), 
+                                   names = c("light_blue", "light_gray", "dark_blue", "orange", 
+                                             "light_yellow", "dark_gray", "lime_green"), 
+                                   as_df = FALSE)
 
 
-# - pal_ucihealth_secondary: Neutral colors ----
 
-#' Colors of the University of California, Irvine
+# - pal_ucihealth_2: Neutral colors ----
+
+#' Neutral colors of the University of California, Irvine
 #' 
-#' \code{pal_ucihealth_secondary} provides the 5 secondary health colors
+#' \code{pal_ucihealth_2} provides the 5 secondary health colors
 #' of the \href{https://www.uci.edu}{University of California Health, Irvine}, USA.
 #'
 #' The 5 neutral colors are
 #' \code{"brick_red"}(defined as HEX #981e32),
 #' \code{"purple"}(defined as HEX #631d76),
 #' \code{"teal_blue"}(defined as HEX #0083b3),
-#' \code{"green"}(defined as HEX #3f9c35) and 
+#' \code{"green"}(defined as HEX #3f9c35), and 
 #' \code{"dark_blue"}(defined as HEX #1b3d6d).
 #' 
-#' \code{pal_ucihealth_secondary} uses the HEX color definition.
+#' \code{pal_ucihealth_2} uses the HEX color definition.
 #'
 #' @return 
 #' A named vector of colors (HEX/HTML codes of type character).
@@ -124,12 +136,12 @@ pal_uci_secondary <- unikn::newpal(col = c("#6aa2b8","#c6beb5","#1b3d6d","#f78d2
 #' \strong{unicol}, 2023-07-13.
 #' 
 #' @source 
-#' Color definitions are based on the 
-#' \href{https://brand.uci.edu/master-branding/color-palette/}
+#' Color definitions are based on 
+#' \href{https://brand.uci.edu/master-branding/color-palette/}{UCI's brand guide}.
 #' 
 #' @examples 
-#' pal_ucihealth_secondary
-#' unikn::seecol(pal_ucihealth_secondary, main = "University of California, Irvine") # view color palette
+#' pal_ucihealth_2
+#' unikn::seecol(pal_ucihealth_2, main = "University of California, Irvine") # view color palette
 #' 
 #' @seealso 
 #' \code{\link{seecol}} for viewing and comparing color palettes;
@@ -140,14 +152,17 @@ pal_uci_secondary <- unikn::newpal(col = c("#6aa2b8","#c6beb5","#1b3d6d","#f78d2
 #' 
 #' @export
 
-pal_ucihealth_secondary <- unikn::newpal(col = c("#981e32","#631d76","#0083b3","#3f9c35","#1b3d6d"), names = c("brick_red","purple","teal_blue","green","dark_blue"), as_df = FALSE)
+pal_ucihealth_2 <- unikn::newpal(col = c("#981e32","#631d76","#0083b3","#3f9c35","#1b3d6d"), 
+                                 names = c("brick_red","purple","teal_blue", "green", "dark_blue"), 
+                                 as_df = FALSE)
 
 
-# - pal_ucihealth_tertiary: Teritary colors ----
 
-#' Colors of the University of California, Irvine
+# - pal_ucihealth_3: Tertiary colors ----
+
+#' Tertiary colors of the University of California, Irvine
 #' 
-#' \code{pal_ucihealth_tertiary} provides the 5 teritary colors of the 
+#' \code{pal_ucihealth_3} provides the 5 teritary colors of the 
 #' of the \href{https://www.uci.edu}{University of California Health, Irvine}, USA.
 #'
 #' The 5 teritary colors are
@@ -157,7 +172,7 @@ pal_ucihealth_secondary <- unikn::newpal(col = c("#981e32","#631d76","#0083b3","
 #' \code{"lime_green"}(defined as HEX #7ab800) and 
 #' \code{"yellow"}(defined as HEX #fecb00).
 #' 
-#' \code{pal_ucihealth_tertiary} uses the HEX color definition.
+#' \code{pal_ucihealth_3} uses the HEX color definition.
 #'
 #' @return 
 #' A named vector of colors (HEX/HTML codes of type character).
@@ -166,12 +181,12 @@ pal_ucihealth_secondary <- unikn::newpal(col = c("#981e32","#631d76","#0083b3","
 #' \strong{unicol}, 2023-07-13.
 #' 
 #' @source 
-#' Color definitions are based on the 
-#' \href{https://brand.uci.edu/master-branding/color-palette/}
+#' Color definitions are based on 
+#' \href{https://brand.uci.edu/master-branding/color-palette/}{UCI's brand guide}.
 #' 
 #' @examples 
-#' pal_ucihealth_tertiary
-#' unikn::seecol(pal_ucihealth_tertiary, main = "University of California, Irvine") # view color palette
+#' pal_ucihealth_3
+#' unikn::seecol(pal_ucihealth_3, main = "University of California, Irvine") # view color palette
 #' 
 #' @seealso 
 #' \code{\link{seecol}} for viewing and comparing color palettes;
@@ -182,7 +197,9 @@ pal_ucihealth_secondary <- unikn::newpal(col = c("#981e32","#631d76","#0083b3","
 #' 
 #' @export
 
-pal_ucihealth_tertiary <- unikn::newpal(col = c("#b71234","#7c109a","#00b0ca","#7ab800","#fecb00"), names = c("dark_orange","bright_purple","turquoise","lime_green","yellow"), as_df = FALSE)
+pal_ucihealth_3 <- unikn::newpal(col = c("#b71234","#7c109a","#00b0ca","#7ab800","#fecb00"), 
+                                 names = c("dark_orange","bright_purple","turquoise","lime_green","yellow"), 
+                                 as_df = FALSE)
 
 
 ## ToDo: -----
