b8b5d04a1e65530f5283856df8b6f3bf512aaf22
hneth <hansneth@gmail.com>
2023-07-18 12:09:27 +0200

    bug fix: RGB colors (as vector)

--- R/Glasgow.R
+++ R/Glasgow.R
@@ -1,4 +1,4 @@
-## Glasgow.R|2023 07 05
+## Glasgow.R | 2023 07 05
 ## Colors of the University of Glasgow, Scotland
 ## ---------------------------------------------
 
@@ -35,7 +35,7 @@
 #' \code{"glasgow_rust"}(defines as HEX #9A3A06),
 #' \code{"glasgow_sandstone"}(defines as HEX #52473B),
 #' \code{"glasgow_sky_blue"}(defines as HEX #005398),
-#' \code{"glasgow_slate"}(defines as HEX #4F5961) and
+#' \code{"glasgow_slate"}(defines as HEX #4F5961), and
 #' \code{"glasgow_thistle"}(defines as HEX #951272).
 #'
 #' \code{pal_glasgow_1}uses the HEX color definition.
@@ -65,43 +65,48 @@
 #'
 #' @export
 
-pal_glasgow_1 <- unikn::newpal(col=c("#003865","#7D2239","#005C8A","#5B4D94","#006630","#385A4F","#B30C00","#9A3A06","#52473B","#005398","#4F5961","#951272"), names= c("glasgow_university_blue","glasgow_burgundy", "glasgow_cobalt","glasgow_lavender","glasgow_leaf","glasgow_moss","glasgow_pillarbox","glasgow_rust","glasgow_sandstone","glasgow_sky_blue","glasgow_slate","glasgow_thistle"), as_df=FALSE)
+pal_glasgow_1 <- unikn::newpal(col = c("#003865", "#7D2239", "#005C8A", "#5B4D94", "#006630", "#385A4F",
+                                       "#B30C00", "#9A3A06", "#52473B", "#005398", "#4F5961", "#951272"), 
+                               names = c("glasgow_university_blue", "glasgow_burgundy", "glasgow_cobalt", "glasgow_lavender", "glasgow_leaf", "glasgow_moss",
+                                         "glasgow_pillarbox", "glasgow_rust", "glasgow_sandstone", "glasgow_sky_blue", "glasgow_slate", "glasgow_thistle"), 
+                               as_df = FALSE)
+
 
 
 # pal_glasgow_2: Secondary colors----
 
-#'Colors of the university of glasgow
+#' Secondary colors of the University of Glasgow, Scotland
 #'
-#'\code{pal_glasgow_2}provides the 5 secondary colors
-#'of the \href{https://www.gla.ac.uk/}{University of Glasgow}, Scotland
+#' \code{pal_glasgow_2}provides the 5 secondary colors 
+#' of the \href{https://www.gla.ac.uk/}{University of Glasgow}, Scotland. 
 #'
-#'The 5 secondary colors are
-#'\code{"glasgow_turquoise"}(defined as R0 G181 B209),
-#'\code{"glasgow_rose"}(defined as R176 G108 B150),
-#'\code{"glasgow_pumpkin"}(defined as R255 G185 B72),
-#'\code{"glasgow_sunshine"}(defined as R255 G220 B54) and
-#'\code{"glasgow_mocha"}(defined as R170 G128 B102).
+#' The 5 secondary colors are 
+#' \code{"glasgow_turquoise"}(defined as R0 G181 B209),
+#' \code{"glasgow_rose"}(defined as R176 G108 B150),
+#' \code{"glasgow_pumpkin"}(defined as R255 G185 B72),
+#' \code{"glasgow_sunshine"}(defined as R255 G220 B54), and
+#' \code{"glasgow_mocha"}(defined as R170 G128 B102).
 #'
-#'\code{pal_glasgow_2} uses the RGB color definition.
+#' \code{pal_glasgow_2} uses the RGB color definition.
 #'
-#'@return
-#'A named vector of colors (RGB/HTML codes of type character).
+#' @return
+#' A named vector of colors (RGB/HTML codes of type character).
 #'
-#'@author
-#'\strong{unicol}, 2023-07-05.
+#' @author
+#' \strong{unicol}, 2023-07-05.
 #'
-#'@source
-#'Color definitions are based on the
-#'\href{gla.ac.uk}
+#' @source
+#' Color definitions are based on 
+#' \href{https://www.gla.ac.uk/myglasgow/staff/brandtoolkit/brandelements/colours/}{UGlasgow's website}.
 #'
-#'@examples
-#'pal_glasgow_2
-#'unikn::seecol(pal_glasgow_2, main="University of Glasgow") #view color palette
+#' @examples
+#' pal_glasgow_2
+#' unikn::seecol(pal_glasgow_2, main="University of Glasgow") #view color palette
 #'
-#'@family university of color palettes
+#' @family university of color palettes
 #'
-#'@seealso
-#'\code{\link{seecol}} for viewing and comparing color palettes;
+#' @seealso
+#' \code{\link{seecol}} for viewing and comparing color palettes;
 #' \code{\link{usecol}} for using color palettes;
 #' \code{\link{simcol}} for finding similar colors;
 #' \code{\link{newpal}} for defining new color palettes;
@@ -109,12 +114,19 @@ pal_glasgow_1 <- unikn::newpal(col=c("#003865","#7D2239","#005C8A","#5B4D94","#0
 #'
 #'@export
 
-pal_glasgow_2 <- unikn::newpal(rgb(0, 181, 209, maxColorValue = 255), rgb(176, 108, 150, maxColorValue = 255), rgb(255, 185, 72, maxColorValue = 255), rgb(255, 220, 54, maxColorValue = 255), rgb(170, 128, 102, maxColorValue = 255), names = c("glasgow_turquoise", "glasgow_rose",
-                                                                                                                                                                                                                                                  "glasgow_pumpkin", "glasgow_sunshine","glasgow_mocha"), as_df = FALSE)
-##Check:
+pal_glasgow_2 <- unikn::newpal(col = c(rgb(0, 181, 209, maxColorValue = 255), 
+                                       rgb(176, 108, 150, maxColorValue = 255), 
+                                       rgb(255, 185, 72, maxColorValue = 255), 
+                                       rgb(255, 220, 54, maxColorValue = 255), 
+                                       rgb(170, 128, 102, maxColorValue = 255)), 
+                               names = c("glasgow_turquoise", "glasgow_rose",
+                                         "glasgow_pumpkin", "glasgow_sunshine", 
+                                         "glasgow_mocha"), 
+                               as_df = FALSE)
+## Check:
 
-#unikn::seecol(pal_glasgow_1, main= "University of Glasgow", col_bg= "glasgow_burgundy")
-#unikn::seecol(pal_glasgow_2, main = "University of Glasgow", col_bg= "glasgow_rose")
+# unikn::seecol(pal_glasgow_1, main= "University of Glasgow", col_bg= "glasgow_burgundy")
+# unikn::seecol(pal_glasgow_2, main = "University of Glasgow", col_bg= "glasgow_rose")
 
 
 ## ToDo: ----
