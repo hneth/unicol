45197cf37c7957a09c59a5d95391cd5056d9420d
hneth <hansneth@gmail.com>
2023-07-21 15:17:20 +0200

    fix bug and rename pals

--- R/Arizona.R
+++ R/Arizona.R
@@ -4,7 +4,7 @@
 
 # Information: ----
 
-# unipals: "pal_asu_primary", "pal_asu_secondary", "pal_asu_grayscale" (3)
+# unipals: "asu_1", "asu_2", "asu_3" (3)
 # inst: "Arizona State University" 
 # country: USA
 # URL: "https://www.asu.edu"
@@ -15,11 +15,11 @@
 
 # Color palettes:
 
-# - pal_asu_primary: Primary colors of ASU ----
+# - asu_1: Primary colors of ASU ----
 
-#' Colors of the Arizona State University:
+#' Primary colors of the Arizona State University, USA 
 #'
-#' \code{pal_asu_primary} provides the 4 primary colors
+#' \code{asu_1} provides the four primary colors
 #' of the \href{https://www.asu.edu}{Arizona State University}, USA.
 #'
 #' The 4 primary colors are
@@ -28,7 +28,7 @@
 #' \code{"asu_rich_black"} (defined as HEX #000000)
 #' \code{"asu_white"} (defined as HEX #FFFFFF)
 #'
-#' \code{pal_asu_primary} uses the HEX color definitions.
+#' \code{asu_1} uses the HEX color definitions.
 #'
 #' @return
 #' A names vector of colors (HEX/HTML codes of type character).
@@ -41,12 +41,14 @@
 #' \href{https://brandguide.asu.edu/brand-elements/design/color}{ASU's brand guide}.
 #'
 #' @examples
-#' pal_asu_primary
-#' unikn::seecol(pal_asu_primary, main= "Arizona State University") #view color palette.
+#' asu_1
+#' unikn::seecol(asu_1, main= "Arizona State University") #view color palette.
 #'
 #' @family university color palettes
 #'
 #' @seealso
+#' \code{\link{asu_2}} for ASU's secondary colors;
+#' \code{\link{asu_3}} for ASU's grayscale colors;
 #' \code{\link{seecol}} for viewing and comparing color palettes;
 #' \code{\link{usecol}} for using color palettes;
 #' \code{\link{simcol}} for finding similar colors;
@@ -55,28 +57,28 @@
 #' 
 #' @export
 
-pal_asu_primary <- unikn::newpal(col = c("#8C1D40", "#FFC627", "#000000", "#FFFFFF"), 
-                                 names = c("asu_maroon", "asu_gold", "black", "white"), 
-                                 as_df = FALSE)
+asu_1 <- unikn::newpal(col = c("#8C1D40", "#FFC627", "#000000", "#FFFFFF"), 
+                       names = c("asu_maroon", "asu_gold", "black", "white"), 
+                       as_df = FALSE)
 
 
-# - pal_asu_secondary: Secondary colors of ASU -----
+# - asu_2: Secondary colors of ASU -----
 
-#' Colors of the Arizona State University
+#' Colors of the Arizona State University, USA 
 #' 
-#' \code{pal_asu_secondary} provides the 7 secondary colors
+#' \code{asu_2} provides the seven secondary colors
 #' of the \href{https://www.asu.edu}{Arizona State University}, USA.
 #' 
 #' The 7 secondary colors are
 #' \code{"asu_green"} (defined as HEX #78BE20),
 #' \code{"asu_blue"} (defined as HEX #00A3E0),
 #' \code{"asu_orange"} (defined as HEX #FF7F32),
-#' \code{"asu_gray"} (defined as HEX #747474)
-#' \code{"asu_copper"} (defined as HEX #Af674B)
-#' \code{"asu_turquoise"} (defined as HEX #4AB7C4)
-#' \code{"asu_pink"} (defined as HEX #E74973)
+#' \code{"asu_gray"} (defined as HEX #747474), 
+#' \code{"asu_copper"} (defined as HEX #Af674B), 
+#' \code{"asu_turquoise"} (defined as HEX #4AB7C4), and 
+#' \code{"asu_pink"} (defined as HEX #E74973). 
 #' 
-#' \code{pal_asu_secondary} uses the HEX color definitions.
+#' \code{asu_2} uses the HEX color definitions.
 #' 
 #' @return 
 #' A names vector of colors (HEX/HTML codes of type character).
@@ -89,12 +91,14 @@ pal_asu_primary <- unikn::newpal(col = c("#8C1D40", "#FFC627", "#000000", "#FFFF
 #' \href{https://brandguide.asu.edu/brand-elements/design/color}{ASU's brand guide}.
 #'
 #' @examples 
-#' pal_asu_secondary
-#' unikn::seecol(pal_asu_secondary, main = "Arizona State University") # view color palette
+#' asu_2
+#' unikn::seecol(asu_2, main = "Arizona State University 2") # view color palette
 #' 
 #' @family university color palettes.
 #'
 #' @seealso 
+#' \code{\link{asu_1}} for ASU's primary colors;
+#' \code{\link{asu_3}} for ASU's grayscale colors;
 #' \code{\link{seecol}} for viewing and comparing color palettes;
 #' \code{\link{usecol}} for using color palettes;
 #' \code{\link{simcol}} for finding similar colors;
@@ -103,21 +107,21 @@ pal_asu_primary <- unikn::newpal(col = c("#8C1D40", "#FFC627", "#000000", "#FFFF
 #' 
 #' @export
 
-pal_asu_secondary <- unikn::newpal(col = c("#78BE20", "#00A3E0", "#FF7F32", "#747474", 
-                                           "#Af674B", "#4AB7C4", "#E74973"), 
-                                   names = c("asu_green", "asu_blue","asu_orange", "asu_gray", 
-                                             "asu_copper", "asu_turquoise", "asu_pink"), 
-                                   as_df = FALSE)
+asu_2 <- unikn::newpal(col = c("#78BE20", "#00A3E0", "#FF7F32", "#747474", 
+                               "#Af674B", "#4AB7C4", "#E74973"), 
+                       names = c("asu_green", "asu_blue","asu_orange", "asu_gray", 
+                                 "asu_copper", "asu_turquoise", "asu_pink"), 
+                       as_df = FALSE)
 
 
-# - pal_asu_grayscale: grayscale colors of ASU -----
+# - asu_3: grayscale colors of ASU -----
 
-#' Colors of the Arizona State University
+#' Grayscale colors of the Arizona State University, USA 
 #' 
-#' \code{pal_asu_grayscale} provides the 9 grayscale colors
+#' \code{asu_3} provides the nine grayscale colors
 #' of the \href{https://www.asu.edu}{Arizona State University}, USA.
 #' 
-#' The 9 tertiary colors are 
+#' The 9 grayscale colors are 
 #' \code{"asu_black"} (defined as HEX #000000),
 #' \code{"gray1"} (defined as HEX #191919),
 #' \code{"gray2"} (defined as HEX #484848),
@@ -128,7 +132,7 @@ pal_asu_secondary <- unikn::newpal(col = c("#78BE20", "#00A3E0", "#FF7F32", "#74
 #' \code{"gray7"} (defined as HEX #FAFAFA), and 
 #' \code{"asu_white"} (defined as HEX #FFFFFF). 
 #' 
-#' \code{pal_asu_grayscale} uses the HEX color definitions.
+#' \code{asu_3} uses the HEX color definitions.
 #' 
 #' @return 
 #' A names vector of colors (HEX/HTML codes of type character).
@@ -141,12 +145,14 @@ pal_asu_secondary <- unikn::newpal(col = c("#78BE20", "#00A3E0", "#FF7F32", "#74
 #' \href{https://brandguide.asu.edu/brand-elements/design/color}{ASU's brand guide}.
 #'
 #' @examples 
-#' pal_asu_grayscale
-#' unikn::seecol(pal_asu_grayscale, main = "Arizona State University") # view color palette
+#' asu_3
+#' unikn::seecol(asu_3, main = "Arizona State University") # view color palette
 #' 
 #' @family university color palettes.
 #'
 #' @seealso 
+#' \code{\link{asu_1}} for ASU's primary colors;
+#' \code{\link{asu_2}} for ASU's secondary colors;
 #' \code{\link{seecol}} for viewing and comparing color palettes;
 #' \code{\link{usecol}} for using color palettes;
 #' \code{\link{simcol}} for finding similar colors;
@@ -155,13 +161,13 @@ pal_asu_secondary <- unikn::newpal(col = c("#78BE20", "#00A3E0", "#FF7F32", "#74
 #' 
 #' @export
 
-pla_asu_grayscale <- unikn::newpal(col = c("#000000", "#191919", "#484848", "#747474", 
-                                           "#BFBFBF", "#D0D0D0", "#E8E8E8", "#FAFAFA", 
-                                           "#FFFFFF"), 
-                                   names = c("black","gray_1", "gray_2","gray_3", 
-                                             "gray_4", "gray_5", "gray_6", "gray_7", 
-                                             "white"), 
-                                   as_df = FALSE)
+asu_3 <- unikn::newpal(col = c("#000000", "#191919", "#484848", "#747474", 
+                               "#BFBFBF", "#D0D0D0", "#E8E8E8", "#FAFAFA", 
+                               "#FFFFFF"), 
+                       names = c("black","gray_1", "gray_2","gray_3", 
+                                 "gray_4", "gray_5", "gray_6", "gray_7", 
+                                 "white"), 
+                       as_df = FALSE)
 
 
 ## ToDo: -----
