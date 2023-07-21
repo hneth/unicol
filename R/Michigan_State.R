3235397f1f8dbc1d0e4085313201f339f08e9725
hneth <hansneth@gmail.com>
2023-07-21 13:13:52 +0200

    bug fixes: syntax and names

--- R/Michigan_State.R
+++ R/Michigan_State.R
@@ -1,46 +1,52 @@
+## Michigan_State.R | 2023 07 21
+## Colors of Michigan State University, USA
+## ----------------------------------------
 
-# Information: 
 
-# uni_pals: "pal_msu"
-# inst: "Michigan State University" (in English)
+# Information: ----
+
+# uni_pals: "msu_pal"
+# inst: "Michigan State University"
 # country: USA
 # URL: "https://msu.edu/"
 
 
 # Color source
-# URL: https://brand.emory.edu/color.html
+# URL: https://brand.msu.edu/visual/color-palette 
 
-# Color palettes
 
-# - MSU1: core colors ------
+# Color palettes:
 
-# Colors of the Michigan State University
+# - msu_pal: 6 core colors ------
+
+# Colors of Michigan State University, USA
 #'
-#' \code {pal_msu} provides the 6 colors of the \href {https://msu.edu/}, USA.
+#' \code{msu_pal} provides the six colors of 
+#' \href{https://msu.edu/}{Michigan State University}, USA.
 #'
-#' The 6 colors are
-#' \code{"Spartan_Green_msu"} (defined as HEX #18453B),
-#' \code{"White"} (defined as HEX #FFFFFF),
-#' \code{"Black"} (defined as HEX #000000),
-#' \code{"kelly_green_msu"} (defined as HEX #008208),
-#' \code{"lime_green_msu"} (defined as HEX #7BBD00), and
-#' \code{"excellence_green_msu"} (defined as HEX #0B9A6D).
+#' The 6 colors are 
+#' \code{"msu_spartan_green"} (defined as HEX #18453B),
+#' \code{"white"} (defined as HEX #FFFFFF),
+#' \code{"black"} (defined as HEX #000000),
+#' \code{"msu_kelly_green"} (defined as HEX #008208),
+#' \code{"msu_lime_green"} (defined as HEX #7BBD00), and
+#' \code{"msu_excellence_green"} (defined as HEX #0B9A6D).
 #'
-#' \code{pal_msu} uses the HEX color definition
+#' \code{msu_pal} uses the HEX color definitions.
 #'
 #' @return
 #' A named vector of colors (HEX/HTML codes of type character).
 #' 
 #' @author 
-#' \strong{unicol}, 2023-07-01
+#' \strong{unicol}, 2023-07-01. 
 #' 
 #' @source 
 #' Color definitions are based on 
 #' \href{https://brand.msu.edu/visual/color-palette}{MSU's brand guide}.
 #' 
 #' @examples 
-#' pal_msu
-#' unikn::seecol(pal_msu, main = "Michigan State University") # view color palette
+#' msu_pal
+#' unikn::seecol(msu_pal, main = "Michigan State University") # view color palette
 #' 
 #' @family university color palettes
 #' 
@@ -53,20 +59,22 @@
 #' 
 #' @export
 
-pal_msu<-unikn::newpal(col = c("#18453B", "#FFFFFF", "#000000", 
-                               "#008208", "#7BBD00", "#0B9A6D"),
-                       names = c("spartan_green_msu", "White","Black", 
-                                 "kelly_green_msu", "lime_green_msu","excellence_green_msu"), 
-                       as_df = FALSE)
+msu_pal <- unikn::newpal(col = c("#18453B", 
+                                 "#FFFFFF", "#000000", 
+                                 "#008208", "#7BBD00", "#0B9A6D"),
+                         names = c("msu_spartan_green", 
+                                   "white","black", 
+                                   "msu_kelly_green", "msu_lime_green","msu_excellence_green"), 
+                         as_df = FALSE)
 
 
 ## Check:
-# unikn::seecol(pal_msu, main = "colors of the Michigan State University", col_bg = "white")
-# unikn::seecol(pal_msu, main = "colors of the Michigan State University", col_bg = "grey")
+# unikn::seecol(msu_pal, main = "colors of the Michigan State University")
 
-#ToDo:-----
 
-#-etc.
+# ToDo:-----
+
+# -etc.
 
 ### eof. -------
 
