bf9d0d6a07328a48d1554811e84ae6e68dd22373
hneth <hansneth@gmail.com>
2023-07-21 13:07:07 +0200

    rename pals

--- R/Minnesota_Twin_Cities.R
+++ R/Minnesota_Twin_Cities.R
@@ -1,9 +1,9 @@
-# Minnesota Twin Cities | 2023-07-01
+# Minnesota Twin Cities | 2023-07-21
 # Colors of the University of Minnesota, USA
 # -------------------------------------------
 
 # Information
-# uni_pals:"pal_minnesotatwin_1", "pal_minnesotatwin_2" (2)
+# uni_pals:"minnesotatwin_1", "minnesotatwin_2" (2)
 # inst: "University of Minnesota Twin Cities"
 # country: USA
 # URL: "https://twin-cities.umn.edu"
@@ -16,18 +16,18 @@
 
 # Color palettes:
 
-# pal_minnesotatwin_1: Primary colors of UMN ------ 
+# minnesotatwin_1: Primary colors of UMN ------ 
 
 #' Primary colors of the University of Minnesota
 #'
-#' \code{pal_minnesotatwin_1} provides the primary colors
+#' \code{minnesotatwin_1} provides the primary colors
 #' of the \href{https://twin-cities.umn.edu/}{University of Minnesota}, USA.
 #'
 #' The 2 primary colors are
 #' \code{"minnesotatwin_maroon"} (defined as HEX #7A0019) and
 #' \code{"minnesotatwin_gold"} (defined as HEX #FFCC33).
 #'
-#' \code{pal_minnesotatwin_1} uses the HEX color definitions.
+#' \code{minnesotatwin_1} uses the HEX color definitions.
 #'
 #' @return
 #' A named vector of colors (HEX/HTML codes of type character).
@@ -40,15 +40,15 @@
 #' \href{https://university-relations.umn.edu/resources/colors-and-type}{UMN website}.
 #'
 #' @examples
-#' pal_minnesotatwin_1
-#' unikn::seecol(pal_minnesotatwin_1, 
+#' minnesotatwin_1
+#' unikn::seecol(minnesotatwin_1, 
 #'               main = "University of Minnesota Twin Cities") # view color palette
 #'
 #'
 #' @family university of color palettes
 #'
 #' @seealso
-#' \code{\link{pal_minnesotatwin_2}} for the secondary colors of UMN;
+#' \code{\link{minnesotatwin_2}} for the secondary colors of UMN;
 #' \code{\link{seecol}} for viewing and comparing color palettes;
 #' \code{\link{usecol}} for using color palettes;
 #' \code{\link{simcol}} for finding similar colors;
@@ -57,18 +57,18 @@
 #'
 #' @export
 
-pal_minnesotatwin_1 <- unikn::newpal(col = c("#7A0019","#FFCC33"), 
-                                     names = c("minnesotatwin_maroon", "minnesotatwin_gold"), 
-                                     as_df = FALSE)
+minnesotatwin_1 <- unikn::newpal(col = c("#7A0019","#FFCC33"), 
+                                 names = c("minnesotatwin_maroon", "minnesotatwin_gold"), 
+                                 as_df = FALSE)
 
 
 
-# pal_minnesotatwin_2: Secondary colors of UMN ------
+# minnesotatwin_2: Secondary colors of UMN ------
 
 
 #' Secondary colors of the University of Minnesota Twin Cities
 #'
-#' \code{pal_minnesotatwin_2} provides the 8 secondary colors
+#' \code{minnesotatwin_2} provides the 8 secondary colors
 #' of the \href{https://twin-cities.umn.edu/}{University of Minnesota Twin Cities}, USA.
 #'
 #' The 8 secondary colors are
@@ -81,7 +81,7 @@ pal_minnesotatwin_1 <- unikn::newpal(col = c("#7A0019","#FFCC33"),
 #' \code{"minnesotatwin_dark_grey"} (defined as HEX #5A5A5A) and
 #' \code{"minnesotatwin_light_grey"} (defined as HEX #D5D6D2).
 #'
-#' \code{pal_minnesotatwin_2} uses the HEX color definitions.
+#' \code{minnesotatwin_2} uses the HEX color definitions.
 #'
 #' @return
 #' A named vector of colors (HEX/HTML codes of type character).
@@ -94,14 +94,14 @@ pal_minnesotatwin_1 <- unikn::newpal(col = c("#7A0019","#FFCC33"),
 #' \href{https://university-relations.umn.edu/resources/colors-and-type}{UMN website}. 
 #'
 #' @examples
-#' pal_minnesotatwin_2
-#' unikn::seecol(pal_minnesotatwin_2, 
+#' minnesotatwin_2
+#' unikn::seecol(minnesotatwin_2, 
 #'               main = "University of Minnesotatwin Twin Cities") # view color palette
 #'
 #' @family university color palettes
 #'
 #' @seealso
-#' \code{\link{pal_minnesotatwin_1}} for the primary colors of UMN;
+#' \code{\link{minnesotatwin_1}} for the primary colors of UMN;
 #' \code{\link{seecol}} for viewing and comparing color palettes;
 #' \code{\link{usecol}} for using color palettes;
 #' \code{\link{simcol}} for finding similar colors;
@@ -110,11 +110,11 @@ pal_minnesotatwin_1 <- unikn::newpal(col = c("#7A0019","#FFCC33"),
 #' 
 #' @export
 
-pal_minnesotatwin_2 <- unikn::newpal(col = c("#5B0013","#FFB71E","#333333", "#777677", 
-                                             "#900021","#FFDE7A","#5A5A5A","#D5D6D2"), 
-                                     names = c("minnesotatwin_dark_marron", "minnesotatwin_dark_gold", "minnesotatwin_darker_grey", "minnesotatwin_medium_grey",
-                                               "minnesotatwin_light_maroon", "minnesotatwin_light_gold", "minnesotatwin_dark_grey", "minnesotatwin_light_grey"), 
-                                     as_df = FALSE)
+minnesotatwin_2 <- unikn::newpal(col = c("#5B0013","#FFB71E","#333333", "#777677", 
+                                         "#900021","#FFDE7A","#5A5A5A","#D5D6D2"), 
+                                 names = c("minnesotatwin_dark_marron", "minnesotatwin_dark_gold", "minnesotatwin_darker_grey", "minnesotatwin_medium_grey",
+                                           "minnesotatwin_light_maroon", "minnesotatwin_light_gold", "minnesotatwin_dark_grey", "minnesotatwin_light_grey"), 
+                                 as_df = FALSE)
 
 
 ## ToDo:------
