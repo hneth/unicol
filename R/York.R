34c97c65c4a841ccefc6010b1c961dcb8d9a8652
hneth <hansneth@gmail.com>
2023-08-24 15:48:18 +0200

    update and rename pals

--- R/York.R
+++ R/York.R
@@ -1,14 +1,14 @@
-## York University.R | 2023 08 16
-## Colors of the University of York, Canada
-## -----------------------------------------------
+## York University.R | 2023 08 24
+## Colors of York University, Canada
+## ---------------------------------
 
 # Information: ------
 
-# cur_pals: "york_1","york_2" (1)
+# cur_pals: "yorku_1","yorku_2" (1)
 # inst: "York University" (in English)
 # inst_alt: "York University"
 # country: Canada
-# URL: "https://www.yorku.ca/"
+# URL: "https://www.yorku.ca"
 
 # Color source:
 
@@ -16,23 +16,24 @@
 # Last check: 2023-06-24
 
 
+# Colors: ------
 
 # Color palette(s):
 
 
-# - york_1: Primary colors ------
+# - yorku_1: Primary colors of York University ------
 
 #' Primary colors of the York University, Canada  
 #'
-#' \code{york_1} provides the three primary colors 
+#' \code{yorku_1} provides the three primary colors 
 #' of the \href{https://www.yorku.ca/}{York University}, Canada.
 #'
-#' The three primary colors are
+#' The 3 primary colors are
 #' \code{"york_red"} (defined as RGB 227/24/55, HEX #E31837),
 #' \code{"white"} (defined as RGB 255/255/255, HEX #FFFFFF), and
 #' \code{"black"} (defined as RGB 0/0/0, HEX #000000).
 #' 
-#' \code{york_1} uses the HEX color definitions. 
+#' \code{yorku_1} uses the HEX color definitions. 
 #'
 #' @return 
 #' A named vector of colors (HEX/HTML codes of type character).
@@ -45,10 +46,10 @@
 #' \href{https://www.yorku.ca/brand/wp-content/uploads/sites/18/2021/07/YORKU_Brand_Standards_July2021.pdf}{York Brand Standards (PDF)}.
 #'
 #' @examples
-#' york_1
-#' unikn::seecol(york_1, main = "Primary colors of York University") # view color palette
+#' yorku_1
+#' unikn::seecol(yorku_1, main = "Primary colors of York University") # view color palette
 #'
-#' @family university color palettes
+#' @family Canadian university color palettes
 #'
 #' @seealso
 #' \code{\link{seecol}} for viewing and comparing color palettes;
@@ -59,18 +60,18 @@
 #'
 #' @export
 
-york_1 <- unikn::newpal(col = c("#E31837", "#FFFFFF", "#000000"),
-                        names = c("york_red", "white", "black"),
-                        as_df = FALSE)
+yorku_1 <- unikn::newpal(col = c("#E31837", "#FFFFFF", "#000000"),
+                         names = c("york_red", "white", "black"),
+                         as_df = FALSE)
 
-# - york_2: secondary colors ------
+# - yorku_2: Secondary colors of York University ------
 
 #' Secondary colors of the York University, Canada  
 #'
-#' \code{york_2} provides the five secondary colors 
+#' \code{yorku_2} provides eight secondary colors 
 #' of the \href{https://www.yorku.ca/}{York University}, Canada.
 #'
-#'
+#' The 8 secondary colors are: 
 #' \code{"york_red_medium"}  (defined as RGB 175/13/26,   HEX #AF0D1A),
 #' \code{"york_red_dark"}    (defined as RGB 129/0/1,     HEX #810001),
 #' \code{"york_bright_blue"} (defined as RGB 58/194/239,  HEX #3AC2EF),
@@ -80,7 +81,7 @@ york_1 <- unikn::newpal(col = c("#E31837", "#FFFFFF", "#000000"),
 #' \code{"york_grey_medium"} (defined as RGB 183/174/169, HEX #B7AEA9), and
 #' \code{"york_grey_dark"}   (defined as RGB 104/98/96,   HEX #686260).
 #' 
-#' \code{york_2} uses the HEX color definitions. 
+#' \code{yorku_2} uses the HEX color definitions. 
 #' 
 #' @return 
 #' A named vector of colors (HEX/HTML codes of type character).
@@ -93,10 +94,10 @@ york_1 <- unikn::newpal(col = c("#E31837", "#FFFFFF", "#000000"),
 #' \href{https://www.yorku.ca/brand/wp-content/uploads/sites/18/2021/07/YORKU_Brand_Standards_July2021.pdf}{York Brand Standards (PDF)}.
 #'
 #' @examples
-#' york_2
-#' unikn::seecol(york_2, main = "York University 2") # view color palette
+#' yorku_2
+#' unikn::seecol(yorku_2, main = "York University 2") # view color palette
 #'
-#' @family university color palettes
+#' @family Canadian university color palettes
 #'
 #' @seealso
 #' \code{\link{seecol}} for viewing and comparing color palettes;
@@ -107,16 +108,16 @@ york_1 <- unikn::newpal(col = c("#E31837", "#FFFFFF", "#000000"),
 #'
 #' @export
 
-york_2 <- unikn::newpal(col = c("#AF0D1A", "#810001", "#3AC2EF", "#ACE6F8", 
-                                "#E1DFDC", "#D6CFCA", "#B7AEA9", "#686260"),
-                        names = c("york_red_medium", "york_red_dark", "bright_blue", "york_light_blue", 
-                                  "york_light_grey", "york_pewter", "york_grey_medium", "york_grey_dark"),
-                        as_df = FALSE)
+yorku_2 <- unikn::newpal(col = c("#AF0D1A", "#810001", "#3AC2EF", "#ACE6F8", 
+                                 "#E1DFDC", "#D6CFCA", "#B7AEA9", "#686260"),
+                         names = c("york_red_medium", "york_red_dark", "bright_blue", "york_light_blue", 
+                                   "york_light_grey", "york_pewter", "york_grey_medium", "york_grey_dark"),
+                         as_df = FALSE)
 
 
 # # Check: 
-# unikn::seecol(york_1, main = "Primary colors of the York University", col_bg = "lightgrey")
-# unikn::seecol(york_2, main = "Secondary colors of the York University", col_bg = "white")
+# unikn::seecol(yorku_1, main = "Primary colors of the York University", col_bg = "lightgrey")
+# unikn::seecol(yorku_2, main = "Secondary colors of the York University", col_bg = "white")
 
 
 ## ToDo: -------- 
