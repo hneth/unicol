34eb08e7232549e3097034e8468749f85f9dfab2
hneth <hansneth@gmail.com>
2023-08-25 14:53:57 +0200

    extract old pals into separate files

---
+++ R/Uni_Hamburg.R
@@ -0,0 +1,193 @@
+## Uni_Hamburg.R | 2023 08 25
+## Colors of the University of Hamburg / Universität Hamburg, Germany 
+## ------------------------------------------------------------------
+
+# This file provides a code frame for existing color palettes.
+
+# Information: ------
+
+# cur_pals: "uni_hamburg_1", "uni_hamburg_12" (2)
+
+# inst: "University of Hamburg" / "Universität Hamburg" 
+# country: Germany 
+# inst_URL: <https://www.uni-hamburg.de>
+
+# Color source: PDF of Corporate Manual („Master Styleguide“) at   
+# color_URL: <https://www.kus.uni-hamburg.de/themen/oeffentlichkeitsarbeit/corporate-design/corporate-manual.html>
+# Last check: 2022-12-06
+
+
+# Colors: ------
+
+# Color definitions:
+
+# GESTALTUNGSKONZEPT (p. 18 of PDF manual)
+# 
+# Die Hauptfarben des Corporate Designs der Universität Hamburg sind 
+# Rot (CMYK 0/100/100/0) und Blau (CMYK 87/49/0/0) – barrierefreier Blauton. 
+# Diese leiten sich zum einen vom Rot des Uni-Logos ab, zum anderen soll das Blau 
+# die Nähe der Stadt Hamburg und damit die Nähe zum Wasser symbolisieren. 
+# Das Blau transportiert ebenso Raum und Perspektive. 
+# Schwarz wird für den Fließtext verwendet. 
+# Als Alternative kann das Steingrau verwendet werden. 
+# Für Tabellen/Diagramme und Organigramme dürfen auch Abstufungen von 
+# Rot, Blau und Steingrau verwendet werden (siehe Seite 25). 
+# 
+# Wichtig:
+# - Blau und Rot sollen subtil eingesetzt werden, nicht zu bunt: 
+#   eher Designelemente wie Linien einfärben, keine ganzen Textpassagen.
+# - Gewichtung der beiden Farben muss gleich sein
+# 
+# Rot       CMYK 0/100/100/0  RGB: 226/0/26   #e2001a  HKS: 14  Pantone: 485C
+# Blau      CMYK 87/49/0/0    RGB: 2/113/187  #0271bb
+# Schwarz   CMYK 0/0/0/100    RGB: 0/0/0      #000000
+# Steingrau CMYK 45/11/11/73  RGB: 59/81/91   #3b515b           Pantone: 432U
+
+hh_rot  <- "#e2001a"
+hh_blau <- "#0271bb"
+hh_steingrau <- "#3b515b"
+
+
+# Schaubilder (p. 25 of PDF manual)
+#
+# Rot CMYK 0/100/100/0 RGB: 226/0/26
+# Rot (mit 50% Deckkraft) CMYK 0/61/35/0 RGB: 240/130/136
+#
+# Blau CMYK 87/49/0/0 RGB: 2/113/187 
+# Blau (mit 50% Deckkraft) CMYK 53/15/7/0 RGB: 128/184/219
+# 
+# Steingrau CMYK 45/11/11/73 RGB: 59/81/91 
+# Steingrau (mit 40% Deckkraft) CMYK 34/21/22/3 RGB: 178/186/189
+# 
+# Achten Sie unbedingt auch auf einen ausreichenden Kontrast, wenn Sie z.B. 
+# Text auf einer Farbfläche platzieren, um die Barrierefreiheit zu gewährleisten. 
+# Weitere Infos dazu: https://www.leserlich.info
+
+rot_50pc       <- rgb(240, 130, 136, maxColorValue = 255)
+blau_50pc      <- rgb(128, 184, 219, maxColorValue = 255)
+steingrau_40pc <- rgb(178, 186, 189, maxColorValue = 255)
+
+
+# Color palette(s):
+
+
+# - uni_hamburg_1: Primary colors of Uni Hamburg ----
+
+#' Primary colors of the University of Hamburg, Germany 
+#'
+#' \code{uni_hamburg_1} provides the four primary colors 
+#' of the \href{https://www.uni-hamburg.de}{University of Hamburg}, Germany. 
+#' 
+#' The two main colors are \code{"rot"} (also defined as Pantone 485C or CMYK 0/100/100/0) 
+#' and \code{"blau"} (also defined as CMYK 87/49/0/0). 
+#' These colors should be weighted equally and in a subtle fashion 
+#' (i.e., as color accents, not large areas or entire texts).
+#' 
+#' The auxiliary colors \code{"black"} and 
+#' \code{"steingrau"} (also defined as Pantone 432U or CMYK 45/11/11/73) 
+#' are used for text.
+#' 
+#' Colors shades may be used in diagrams or visualizations. 
+#' See examples and the \code{\link{ac}} and \code{\link{usecol}} functions 
+#' for creating color gradients.
+#' 
+#' @return 
+#' A named vector of colors (HEX/HTML codes of type character).
+#' 
+#' @author 
+#' \strong{unicol}, 2022-12-06.
+#' 
+#' @source 
+#' Color definitions are based on the 
+#' \href{https://www.kus.uni-hamburg.de/themen/oeffentlichkeitsarbeit/corporate-design/corporate-manual.html}{CD manual}. 
+#' 
+#' @examples
+#' uni_hamburg_1
+#' unikn::seecol(uni_hamburg_1, main = "Primary colors of the University of Hamburg")
+#' unikn::demopal(uni_hamburg_1, type = 3, main = "Primary colors of the Uni Hamburg")
+#' 
+#' # Color gradients:
+#' unikn::seecol(unikn::usecol(c(uni_hamburg_1[1], uni_hamburg_1[2]), n = 6), 
+#'               main = "HHU rot to blau")
+#' unikn::seecol(unikn::usecol(c(uni_hamburg_1[1], "white", uni_hamburg_1[2]), n = 7), 
+#'               main = "Divergent HHU")
+#'
+#' @family German university color palettes
+#'
+#' @seealso 
+#' \code{\link{uni_hamburg_2}} for visualization colors of the University of Hamburg;  
+#' \code{\link{seecol}} for viewing and comparing color palettes; 
+#' \code{\link{usecol}} for using color palettes; 
+#' \code{\link{simcol}} for finding similar colors; 
+#' \code{\link{newpal}} for defining new color palettes; 
+#' \code{\link{grepal}} for finding named colors. 
+#' 
+#' @export
+
+uni_hamburg_1 <- unikn::newpal(col = c(hh_rot, hh_blau, "black", hh_steingrau),
+                               names = c("rot", "blau", "schwarz", "steingrau"),
+                               as_df = FALSE)
+
+# unikn::seecol(uni_hamburg_1, main = "Primary colors of Uni Hamburg")
+
+# # See gradients:
+# unikn::seecol(unikn::usecol(c(uni_hamburg_1[1], uni_hamburg_1[2]), n = 7))
+# unikn::seecol(unikn::usecol(c(uni_hamburg_1[1], "white", uni_hamburg_1[2]), n = 7))
+
+
+
+
+
+# - uni_hamburg_2: Pair-wise colors / for visualizations of Uni Hamburg ----
+
+#' Secondary colors of the University of Hamburg, Germany 
+#'
+#' \code{uni_hamburg_2} provides three primary colors 
+#' of the \href{https://www.uni-hamburg.de}{University of Hamburg}, Germany, 
+#' in combination with two pair-wise variants of them. 
+#' 
+#' \code{uni_hamburg_2} is to be used in visualizations. 
+#' 
+#' @return 
+#' A named vector of colors (HEX/HTML codes of type character).
+#' 
+#' @author 
+#' \strong{unicol}, 2022-12-06.
+#' 
+#' @source 
+#' Color definitions are based on the 
+#' \href{https://www.kus.uni-hamburg.de/themen/oeffentlichkeitsarbeit/corporate-design/corporate-manual.html}{CD manual}. 
+#' 
+#' @examples
+#' uni_hamburg_2
+#' unikn::seecol(uni_hamburg_2, main = "Visualization colors of the University of Hamburg")
+#' unikn::demopal(uni_hamburg_2, type = 1, main = "Secondary colors of the Uni Hamburg")
+#'
+#' @family German university color palettes
+#'
+#' @seealso 
+#' \code{\link{uni_hamburg_1}} for primary colors of the University of Hamburg;  
+#' \code{\link{seecol}} for viewing and comparing color palettes; 
+#' \code{\link{usecol}} for using color palettes; 
+#' \code{\link{simcol}} for finding similar colors; 
+#' \code{\link{newpal}} for defining new color palettes; 
+#' \code{\link{grepal}} for finding named colors. 
+#' 
+#' @export
+
+uni_hamburg_2 <- unikn::newpal(col = c(hh_rot, rot_50pc, hh_blau, blau_50pc, hh_steingrau, steingrau_40pc),
+                               names = c("rot", "rot 50%", "blau", "blau 50%", "steingrau", "steingrau 40%"),
+                               as_df = FALSE)
+
+# unikn::seecol(uni_hamburg_2, main = "Pair-wise colors of Uni Hamburg")
+
+
+
+
+
+
+## ToDo: -----
+
+# - etc. ----
+
+## eof. ----
---
+++ R/Uni_Jena.R
@@ -0,0 +1,229 @@
+## Uni_Jena.R | 2023 08 25
+## Colors of the University of Jena / Universität Jena, Germany
+## ------------------------------------------------------------
+
+
+# Information: ------
+
+# cur_pals: "uni_jena_1", "uni_jena_2" (2)
+
+# inst: "University of Jena" / "Friedrich-Schiller-Universität Jena"
+# country: Germany 
+# inst_URL: <https://www.uni-jena.de>
+
+# Color source: HTML at
+# color_URL: <https://www.uni-jena.de/corporate-design>
+# Last check: 2022-12-31
+
+# Motto: "Light, life, liberty – connecting visions"
+
+
+# Colors: ------
+
+# Color definitions (see below). 
+
+# Farbsystem
+#
+# Die Hausfarbe der Universität Jena ist Dunkelblau, 
+# als Auszeichnungsfarbe wird Gold verwendet.
+
+# Die Auszeichnung der Fakultäten erfolgt durch das etablierte Farbschema. 
+# Es spiegelt sich unter anderem im Baustein »Regenbogenband« des Corporate Designs wider. 
+# Das Regenbogenband wird dann dekorativ eingesetzt, wenn alle Einrichtungen angesprochen werden 
+# oder selbst »sprechen«.
+
+# Hausfarben
+
+# Hausfarbe Dunkelblau 	
+# CMYK: 100, 70, 10, 50
+# RGB: 0, 47, 93
+# HKS: 41 K
+
+jena_dunkelblau <- rgb(0, 47, 93, maxColorValue = 255)
+
+# Auszeichnungsfarbe Gold	
+# CMYK: 20, 25, 60, 25
+# RGB: 174, 154, 99
+# HKS: 98 K
+
+jena_gold <- rgb(174, 154, 99, maxColorValue = 255)
+
+
+# Color palette(s):
+
+# - uni_jena_1: Primary colors of the University of Jena ------
+
+#' Primary colors of the University of Jena, Germany 
+#'
+#' \code{uni_jena_1} provides the main \code{dunkelblau} color 
+#' and the auxiliary \code{gold} color 
+#' of the \href{https://www.uni-jena.de}{University of Jena}, Germany. 
+#' 
+#' The main color \code{dunkelblau} is alternatively defined as 
+#' \code{RGB 0/47/93}, \code{CMYK 100/70/10/50}, or \code{HKS 41 K}. 
+#' 
+#' The auxiliary color \code{gold} is alternatively defined as 
+#' \code{RGB 174/154/99}, \code{CMYK 20/25/60/25}, or \code{HKS 98 K}. 
+#' 
+#' See the \code{\link{ac}} and \code{\link{usecol}} functions 
+#' for creating color gradients. 
+#' 
+#' The full name of the \href{https://www.uni-jena.de}{University of Jena} 
+#' is Friedrich-Schiller-Universität Jena. 
+#' 
+#' @return 
+#' A named vector of colors (HEX/HTML codes of type character).
+#' 
+#' @author 
+#' \strong{unicol}, 2023-01-02.
+#' 
+#' @source 
+#' Color definitions are based on 
+#' \url{https://www.uni-jena.de/corporate-design}. 
+#' 
+#' @examples
+#' uni_jena_1
+#' unikn::seecol(uni_jena_1, main = "Primary colors of the University of Jena")
+#' unikn::demopal(uni_jena_1, type = 2, seed = 5, main = "Primary colors of Jena University")
+#' 
+#' # Gradient: Mixing the main colors with "white":
+#' jena_mix <- unikn::usecol(c(uni_jena_1[1], "white", uni_jena_1[2]), n = 7)
+#' unikn::seecol(jena_mix, main = "A gradient of the University of Jena")
+#'
+#' @family German university color palettes
+#'
+#' @seealso 
+#' \code{\link{uni_jena_2}} for departmental colors of the University of Jena;  
+#' \code{\link{seecol}} for viewing and comparing color palettes; 
+#' \code{\link{usecol}} for using color palettes; 
+#' \code{\link{simcol}} for finding similar colors; 
+#' \code{\link{newpal}} for defining new color palettes; 
+#' \code{\link{grepal}} for finding named colors. 
+#' 
+#' @export
+
+uni_jena_1 <- unikn::newpal(col = c(jena_dunkelblau, jena_gold),
+                            names = c("dunkelblau", "gold"),
+                            as_df = FALSE)
+
+
+# Fakultätsfarben
+
+# Theologische Fakultät
+# CMYK: 80, 85, 0, 0
+# RGB: 87, 61, 144
+
+jena_theologie <- rgb(87, 61, 144, maxColorValue = 255)
+
+# Rechtswissenschaftliche Fakultät
+# CMYK: 15, 100, 25, 15
+# RGB: 185, 8, 95
+
+jena_rechtswiss <- rgb(185, 8, 95, maxColorValue = 255)
+
+# Wirtschaftswissenschaftliche Fakultät:
+# CMYK: 5, 40, 100, 10
+# RGB: 220, 152, 0
+
+jena_wirtschaftswiss <- rgb(220, 152, 0, maxColorValue = 255)
+
+# Philosophische Fakultät:
+# CMYK: 50, 100, 0, 10
+# RGB: 138, 24, 120
+
+jena_philosophie <- rgb(138, 24, 120, maxColorValue = 255)
+
+# Fakultät für Sozial- und Verhaltenswissenschaften:
+# CMYK: 70, 20, 0, 20
+# RGB: 55, 141, 188
+
+jena_sozialverhaltenwiss <- rgb(55, 141, 188, maxColorValue = 255)
+
+# Fakultät für Mathematik und Informatik:
+# CMYK: 100, 25, 40, 10
+# RGB: 0, 122, 139
+
+jena_matheinformatik <- rgb(0, 122, 139, maxColorValue = 255)
+
+# Physikalisch-Astronomische Fakultät:
+# CMYK: 10, 70, 100, 0
+# RGB: 220, 101, 19
+
+jena_physikastronomie <- rgb(220, 101, 19, maxColorValue = 255)
+
+# Chemisch-Geowissenschaftliche Fakultät:
+# CMYK: 70, 10, 100, 5
+# RGB: 86, 158, 49
+
+jena_chemiegeowiss <- rgb(86, 158, 49, maxColorValue = 255)
+
+# Fakultät für Biowissenschaften:
+# CMYK: 90, 30, 100, 10
+# RGB: 0, 119, 54
+
+jena_biowiss <- rgb(0, 119, 54, maxColorValue = 255)
+
+# Medizinische Fakultät:
+# CMYK: 0, 95, 80, 15
+# RGB: 203, 34, 41
+
+jena_medizin <- rgb(203, 34, 41, maxColorValue = 255)
+
+jena_faculties <- c(jena_theologie, jena_rechtswiss, jena_wirtschaftswiss, jena_philosophie, 
+                    jena_sozialverhaltenwiss, jena_matheinformatik, jena_physikastronomie, 
+                    jena_chemiegeowiss, jena_biowiss, jena_medizin)
+
+jena_faculty_names <- c("Theologie", "Rechtswissenschaft", "Wirtschaftswissenschaften", "Philosophie", 
+                        "Sozial- und Verhaltenswissenschaften", "Mathematik und Informatik", "Physik und Astronomie", 
+                        "Chemie und Geowissenschaften", "Biowissenschaften", "Medizin")
+
+
+# - uni_jena_2: Departmental colors of the University of Jena ------
+
+#' Departmental colors of the University of Jena, Germany 
+#'
+#' \code{uni_jena_2} provides the 10 departmental colors 
+#' of the (10 faculties at the)
+#' \href{https://www.uni-jena.de}{University of Jena}, Germany. 
+#' 
+#' The full name of the \href{https://www.uni-jena.de}{University of Jena} 
+#' is Friedrich-Schiller-Universität Jena. 
+#' 
+#' @return 
+#' A named vector of colors (HEX/HTML codes of type character).
+#' 
+#' @author 
+#' \strong{unicol}, 2023-01-02.
+#' 
+#' @source 
+#' Color definitions are based on 
+#' \url{https://www.uni-jena.de/corporate-design}. 
+#' 
+#' @examples
+#' uni_jena_2
+#' unikn::seecol(uni_jena_2, main = "Departmental colors of the University of Jena")
+#' unikn::demopal(uni_jena_2, type = 5, main = "Faculty colors of Jena University")
+#'
+#' @family German university color palettes
+#'
+#' @seealso 
+#' \code{\link{uni_jena_1}} for primary colors of the University of Jena;  
+#' \code{\link{seecol}} for viewing and comparing color palettes; 
+#' \code{\link{usecol}} for using color palettes; 
+#' \code{\link{simcol}} for finding similar colors; 
+#' \code{\link{newpal}} for defining new color palettes; 
+#' \code{\link{grepal}} for finding named colors. 
+#' 
+#' @export
+
+uni_jena_2 <- unikn::newpal(col = jena_faculties,
+                            names = jena_faculty_names, 
+                            as_df = FALSE)
+
+
+
+## ToDo: -----
+
+# - etc. ----
+
+## eof. ----
