ed3beb2e47da443113e48c418cf19414ad3a6bee
hneth <hansneth@gmail.com>
2023-09-20 18:29:10 +0200

    improve util functions

--- R/5_util.R
+++ R/5_util.R
@@ -39,24 +39,73 @@ get_url <- function(pal){
 
 
 
-# get_inst_link: Create a link to inst url from pal name (as character): ---- 
+# look_up: Generalization of get_fns (above): ---- 
 
-get_inst_link <- function(pal){
+look_up <- function(x, data, v_1 = 1, v_2 = 1){
   
-  # positions of pal:
-  pos <- match(pal, unicol_data$pal)
+  # identify vectors:
+  data_v_1 <- data[[v_1]]  # input
+  data_v_2 <- data[[v_2]]  # output
   
-  # lookup corresponding inst and url:
-  inst <- unicol_data$inst[pos]  
-  url  <- unicol_data$url[pos]
+  # positions matching x in input:
+  pos <- match(x, data_v_1)
   
-  paste0("[", inst, "](", url, ")")
+  # use pos to index output:
+  data_v_2[pos]
   
-} # get_inst_link().
+}
 
 # # Check:
-# get_inst_link(pal = c("cardiff_1", "not there", "princeton_1"))
-# get_inst_link(unicol_data$pal)
+# # (a) specific exemplars:
+# look_up(x = c("cardiff_1", "not there", "princeton_1"), data = unicol_data, v_1 = "pal", v_2 = "inst")
+# look_up(x = c("cardiff_1", "not there", "princeton_1"), data = unicol_data, v_1 = "pal", v_2 = "url")
+# # (b) entire column:
+# look_up(x = unicol_data$pal, data = unicol_data, v_1 = "pal", v_2 = "inst")
+# look_up(x = unicol_data$pal, data = unicol_data, v_1 = "pal", v_2 = "url")
+
+
+
+# link_inst: Create a link to institution (given a vector of pal names): ----
+
+link_inst <- function(pal){
+
+  # look up inst and url corresponding to pal:  
+  inst_names <- look_up(pal, unicol_data, "pal", "inst")
+  inst_urls  <- look_up(pal, unicol_data, "pal", "url")  
+  
+  # create link:
+  paste0("[", inst_names, "](", inst_urls, ")")
+  
+} # link_inst().
+
+# # Check:
+# link_inst(pal = c("cardiff_1", "not there", "princeton_1"))
+# link_inst(unicol_data$pal)
+
+
+# link_doc: Create a link to online documentation (given a vector of pal names): ----
+
+link_github_doc <- function(pal){
+  
+  base_url <- "https://hneth.github.io/unicol/reference/"      # in release
+  base_url <- "https://hneth.github.io/unicol/dev/reference/"  # dev version
+  
+  # # look up inst and url corresponding to pal:  
+  # inst_names <- look_up(pal, unicol_data, "pal", "inst")
+  # inst_urls  <- look_up(pal, unicol_data, "pal", "url")  
+  
+  prefix <- base_url
+  suffix <- ".html"
+  
+  # create link:
+  paste0("[", pal, "](", prefix, pal, suffix, ")")
+  
+} # link_github_doc().
+
+# # Check:
+# link_github_doc(pal = c("cardiff_1", "not there", "princeton_1"))
+# link_github_doc(unicol_data$pal)
+
 
 
 
