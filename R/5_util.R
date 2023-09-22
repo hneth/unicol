2b406723f429a34162b354d3f0165fc2019fae08
Hansjörg Neth <hansneth@gmail.com>
2023-09-22 12:32:20 +0200

    add notes

--- R/5_util.R
+++ R/5_util.R
@@ -44,18 +44,23 @@ get_url <- function(pal){
 
 lookup <- function(x, df, v_1 = 1, v_2 = 1){
   
-  # identify vectors in df:
+  # identify vectors in df (for numeric OR name inputs):
   df_v_1 <- df[[v_1]]  # input  / origin
   df_v_2 <- df[[v_2]]  # output / target
   
-  # # (1) FIRST match of x in input:
-  # match_pos <- match(x, df_v_1)  # yields numeric index
+  # # (A) FIRST match of x in input:
+  # match_pos <- match(x = x, table = df_v_1)  # yields numeric index
   # 
   # # index output:
   # df_v_2[match_pos]
   
-  # (2) Allowing for multiple matches:
-  match_log <- df_v_1 %in% x  # yields logical index
+  # (B) Allowing for multiple matches:
+  # match_log <- df_v_1 %in% x  # yields logical index, is identical to:
+  match_log <- match(x = df_v_1, table = x, nomatch = 0) > 0  # yields logical index
+  
+  # (C) Allowing for partial matches:
+  # match_log <- pmatch(x = df_v_1, table = x, nomatch = 0, duplicates.ok = TRUE) > 0  # does not work
+  # match_log <- charmatch(x = df_v_1, table = x, nomatch = 0) > 0  # does not work
   
   # index output:
   df_v_2[match_log]
@@ -64,20 +69,25 @@ lookup <- function(x, df, v_1 = 1, v_2 = 1){
 
 
 # # Check:
-# # (a) specific exemplars:
+# # (A) 1: specific exemplars:
 # lookup(x = c("cardiff_1", "not there", "princeton_1"), unicol_data, v_1 = "pal", v_2 = "inst")
 # lookup(x = c("cardiff_1", "not there", "princeton_1"), unicol_data, v_1 = "pal", v_2 = "url")
 # 
-# # (b) entire column:
+# # (A) 2: entire columns:
 # lookup(x = unicol_data$pal, unicol_data, v_1 = "pal", v_2 = "inst")
 # lookup(x = unicol_data$pal, unicol_data, v_1 = "pal", v_2 = "url")
 # 
-# # (c) 1 input with MANY outputs:
-# lookup(x = c("University of Waterloo", "nowhere", "McGill University"), unicol_data, v_1 = "inst", v_2 = "pal")
-# lookup(x = c("University of Waterloo", "nowhere", "McGill University"), unicol_data, v_1 = "inst", v_2 = "url")
-
-# # (d) Partial matches DO NOT work:
+# # (B) from 1 input to MANY outputs:
+# lookup(x = c("University of Waterloo", "no where", "McGill University"), unicol_data, v_1 = "inst", v_2 = "pal")
+# lookup(x = c("University of Waterloo", "no where", "McGill University"), unicol_data, v_1 = "inst", v_2 = "url")
+# 
+# # (C) Note that partial matches DO NOT work:
 # lookup(x = c("Waterloo", "nowhere", "McGill"), unicol_data, v_1 = "inst", v_2 = "url")
+# 
+# # Towards partial matches:
+# pmatch(c("Waterloo", "not there", "McGill"), unicol_data$inst)
+# charmatch(c("Waterloo", "not there", "McGill"), unicol_data$inst)
+# grepl(c("Waterloo", "not there", "McGill"), unicol_data$inst)  # ToDo: Turn x into regex pattern. 
 
 
 
@@ -139,14 +149,43 @@ n_col <- function(pal_name){
 
 
 
+# country_url: Determine country from URL (assuming url as text) ----
+
+country_url <- function(url){
+  
+  # regmatches(x = url, gregexpr(pattern = "/^(https?://)?([^@/]+@)?[a-z0-9.]+\\.([a-z]{2,4})(:[0-9]+)?/?$/", text = url))
+  
+  regmatches(x = url, gregexpr(pattern = 'https?://[^/]*\\.([^/]+)', text = url))
+  
+  
+  # ToDo: Use multiple steps: +++ here now +++ 
+  
+  # A: Get country code from URL: 
+  # 1. remove leading "https://" or "http://"
+  # 2. find next "/" (or end)
+  # 3. get group before next "/" (or end)
+  
+  # B: Lookup country code in df-table (or switch())
+  
+  
+} # country_url().
+
+# Check:
+# country_url("https://www.uni-konstanz.de")
+
+# Note:
+# basename("https://www.uni-konstanz.de/test.html")
+
+
+
 ## ToDo: --------
 
-# - obtain country/region from URL, using lookup table
+# - obtain country/region from URL, using regex & lookup table/switch 
 
 # - create a more informative summary table
-# - add a country field (or get/read from URL)?
-# - add a variable for number of colors (length)
+#   - add a country field (or get/read from URL)?
+#   - add a variable for number of colors (length)
 
-# - create an interactive game: "Guess the university" (based on 1:n colors)
+# - Idea: create an interactive game: "Guess the university" (based on 1:n colors)
 
 ## eof. ----------
