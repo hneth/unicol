139e4adfde07f7a6256759c2db4b6c4f3e3073c3
Hansjörg Neth <hansneth@gmail.com>
2023-10-14 13:35:20 +0200

    minor

--- R/2_example_pal.R
+++ R/2_example_pal.R
@@ -1,4 +1,4 @@
-## .R | 2023 10 13
+## .R | 2023 10 14
 ## Colors of the University of Example, Country 
 ## --------------------------------------------
 
@@ -16,10 +16,10 @@
 
 # color_URL: Visual identity and brand guidelines at 
 # <https://example.org>
-# Last check: 2023-10-13
+# Last check: 2023-10-14
 
 # Author information:
-# unicol, 2023-10-13
+# unicol, 2023-10-14
 
 
 # Colors: ------
@@ -55,7 +55,7 @@ col_4 <- "black"    # "too dark"
 #' A named vector of colors (HEX/HTML codes of type character).
 #'
 #' @author 
-#' \strong{unicol}, 2023-10-13.
+#' \strong{unicol}, 2023-10-14.
 #'
 #' @source 
 #' Color definitions are based on the 
