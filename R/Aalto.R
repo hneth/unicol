29094412f9bda2f26cf0fa0a496d0410caeb91cd
Hansjörg Neth <hansneth@gmail.com>
2023-10-14 20:20:13 +0200

    add colors and source info

---
+++ R/Aalto.R
@@ -0,0 +1,132 @@
+## Aalto.R | 2023 10 14
+## Colors of Aalto University, Finland
+## -----------------------------------
+
+# This file provides a code frame for existing/old color palettes.
+
+# Information: ------
+
+# cur_pals: "aalto_1" "aalto_2" (2)
+
+# inst: "Aalto University"
+# inst_alt: "Aalto Yliopisto"
+# country: Finland
+# inst_URL: <https://www.aalto.fi> OR <https://www.aalto.fi/en>
+
+# Color source: HTML of <https://brand.aalto.fi/>:
+# color_URL: <https://brand.aalto.fi/visual-identity/about/colour-palette>
+# Last check: 2023-10-14
+
+
+
+
+# Colors: ------
+
+# Color definitions: 
+
+# From <https://brand.aalto.fi/visual-identity/about/colour-palette>
+# Last check: 2023-10-14. 
+
+# Colors:
+
+# Aalto’s brand colours are yellow, red and blue, used together with black and white. 
+# 
+# In addition to the three primary brand colours, the six schools have their own respective signature colours. A school colour should only be used in visual material representing the school in question.
+# 
+# Pantone (PMS), 4-colour print (CMYK), and screen (RGB and HEX) values for each colour are shown below.
+# 
+# Brand colours palette
+
+
+# 1: University brand colours ---- 
+# 
+# The university colours are the primary brand colours to be used in university-level materials. You should always employ one of these colours when creating materials that contain the Aalto University logo.
+# 
+# For consistency and clarity, it is also recommended to use the same brand colour throughout the entire document or publication regardless of media.
+# 
+# Aalto University Blue
+# HEX - "#46A5FF"
+# PMS - 2925
+# CMYK - 85 21 0 0
+# 
+# Aalto University Red
+# HEX - "#FD6360"
+# PMS - 1787
+# CMYK - 0 82 53 0
+# 
+# Aalto University Yellow
+# HEX - "#F7E159"
+# PMS - 106
+# CMYK - 0 0 75 0
+
+aa_cols_1 <- c("#46A5FF", "#FD6360", "#F7E159")
+aa_nams_1 <- paste("Aalto University", c("blue", "red", "yellow"))
+
+
+# 2: School brand colours ----
+# 
+# When creating materials for a certain school, you should always use the designated school brand colour along with black and white. For example, for the School of Science, it is always orange. Never mix schools and their colours.
+# 
+# School of Arts, Design and Architecture
+# HEX - "#FFC341"
+# PMS - 1235
+# CMYK - 0 31 98 0
+# 
+# School of Engineering
+# HEX - "#BB16A3"
+# PMS - 247
+# CMYK - 27 90 0 0
+# 
+# School of Electrical Engineering
+# HEX - "#A987FF"
+# PMS - 2715
+# CMYK - 56 52 0 0
+# 
+# School of Business
+# HEX - "#9BD84C"
+# PMS - 375
+# CMYK - 46 0 90 0
+# 
+# School of Chemical Engineering
+# HEX - "#5DD089"
+# PMS - 339
+# CMYK - 84 0 59 0
+# 
+# School of Science
+# HEX - "#FF8D4F"
+# PMS - 164
+# CMYK - 0 59 80 0
+
+aa_cols_2 <- c("#FFC341", "#BB16A3", "#A987FF", 
+               "#9BD84C", "#5DD089", "#FF8D4F")
+aa_nams_2 <- paste("School of", 
+                   c("Arts, Design and Architecture", "Engineering", "Electrical Engineering", 
+                     "Business", "Chemical Engineering", "Science"))
+
+
+# 3: Supportive palette / Key colours ---- 
+# 
+# Together with university and school brand colours, black and white with their grayscale tints are the key to a successful implementation of our design system.
+# 
+# As black and white not only perform well in any context or with any accent colour, they also possess the best visual contrast that makes them highly accessible as a pair. 
+# 
+# 
+# Accessibility
+# 
+# Colour accessibility with white or black
+# 
+# This chart displays the brand colours of Aalto University and their accessibility when used together with black or white. The small dot represents sufficient contrast pair (either black or white) with the individual accent colour.
+# 
+# For sufficient contrast, there is a darker shade of purple to be used together with white type.
+
+
+# Color palette(s):
+
+
+# - pal_1: Primary pal ------ 
+
+## ToDo: -----
+
+# - etc. ----
+
+## eof. ----
---
+++ R/Helsinki.R
@@ -0,0 +1,91 @@
+## Helsinki.R | 2023 10 14
+## Colors of the University of Helsinki, Finland
+## ---------------------------------------------
+
+# This file provides a code frame for existing/old color palettes.
+
+# Information: ------
+
+# cur_pals: "x", "y" (2)
+
+# inst: "University of Helsinki"
+# inst_alt: "Helsingin Yliopisto / UH"
+# country: Finland
+# inst_URL: <https://www.helsinki.fi> or <https://www.helsinki.fi/en>
+
+# Color source: HTML: / PDF 
+# color_URL: <https://static.helsinki.fi/ds/tokens/color.html>
+# Last check: 2023-10-14
+
+
+# Colors: ------
+
+# Color definitions: 
+
+# From <https://static.helsinki.fi/ds/tokens/color.html>
+# Last check: 2023-10-14. 
+
+# Colors
+
+# The main colors of Helsinki University style are white, black and shades of blue 
+# defined in the brand guide. 
+# 
+# Additional colors are used only as the accent color for different departments designs 
+# that the color is assigned to and should not be used in general components. 
+# 
+# Usage
+# 
+# Color 	CSS variable 	Color code 	Comments
+# --brand-main 	#0e688b 	Main brand color
+# --brand-main-light 	#107eab 	
+# --brand-main-active 	#005379 	
+# --brand-main-dark 	#003146 	
+# --brand-main-nearly-black 	#000222 	
+# 
+# --grayscale-white 	#fff 	Main white
+# --grayscale-light 	#f8f8f8 	
+# --grayscale-medium-dark 	#979797 	
+# --grayscale-dark 	#555555 	
+# --grayscale-black 	#000000 	Main black
+# 
+# --additional-red-light 	#E5053A 	Color reserved for department use
+# --additional-red-dark 	#A31621 	Color reserved for department use
+# --additional-purple-light 	#42003 	Color reserved for department use
+# --additional-yellow 	#F9A21A 	Color reserved for department use
+# --additional-skyblue 	#48C5F8 	Color reserved for department use
+# --additional-green-light 	#96BA3C 	Color reserved for department use
+# --additional-green-dark 	#006400 	Color reserved for department use
+
+"#0e688b" # brand main
+"#107eab" # .. light
+"#005379" # .. active
+"#003146" # .. dark
+"#000222" # .. nearly-black
+
+"#ffffff" #	white
+"#f8f8f8" # grayscale-light 	
+"#979797" # grayscale-medium-dark 
+"#555555" # grayscale-dark
+"#000000" # black
+
+# Colors reserved for departmental use:
+"#E5053A" # --additional-red-light
+"#A31621" # --additional-red-dark
+"#420030" # --additional-purple-light
+"#F9A21A" # --additional-yellow
+"#48C5F8" # --additional-skyblue
+"#96BA3C" # --additional-green-light
+"#006400" # --additional-green-dark
+
+
+# Color palette(s):
+
+
+
+# - pal_1: Primary pal ------ 
+
+## ToDo: -----
+
+# - etc. ----
+
+## eof. ----
