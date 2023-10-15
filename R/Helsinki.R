9e37438bf33c13f22baed00711b1a665e230b2f1
Hansjörg Neth <hansneth@gmail.com>
2023-10-15 11:25:51 +0200

    define colors and names

--- R/Helsinki.R
+++ R/Helsinki.R
@@ -1,4 +1,4 @@
-## Helsinki.R | 2023 10 14
+## Helsinki.R | 2023 10 15
 ## Colors of the University of Helsinki, Finland
 ## ---------------------------------------------
 
@@ -48,34 +48,92 @@
 # --grayscale-dark 	#555555 	
 # --grayscale-black 	#000000 	Main black
 # 
-# --additional-red-light 	#E5053A 	Color reserved for department use
-# --additional-red-dark 	#A31621 	Color reserved for department use
-# --additional-purple-light 	#42003 	Color reserved for department use
-# --additional-yellow 	#F9A21A 	Color reserved for department use
-# --additional-skyblue 	#48C5F8 	Color reserved for department use
+# --additional-red-light 	  #E5053A 	Color reserved for department use
+# --additional-red-dark 	  #A31621 	Color reserved for department use
+# --additional-purple-light #42003 	  Color reserved for department use
+# --additional-yellow 	    #F9A21A 	Color reserved for department use
+# --additional-skyblue 	    #48C5F8 	Color reserved for department use
 # --additional-green-light 	#96BA3C 	Color reserved for department use
 # --additional-green-dark 	#006400 	Color reserved for department use
 
-"#0e688b" # brand main
-"#107eab" # .. light
-"#005379" # .. active
-"#003146" # .. dark
-"#000222" # .. nearly-black
 
-"#ffffff" #	white
-"#f8f8f8" # grayscale-light 	
-"#979797" # grayscale-medium-dark 
-"#555555" # grayscale-dark
-"#000000" # black
+# 1: Brand main colors ----
+# 
+# "#0e688b" # brand main
+# "#107eab" # .. light
+# "#005379" # .. active
+# "#003146" # .. dark
+# "#000222" # .. nearly-black
 
-# Colors reserved for departmental use:
-"#E5053A" # --additional-red-light
-"#A31621" # --additional-red-dark
-"#420030" # --additional-purple-light
-"#F9A21A" # --additional-yellow
-"#48C5F8" # --additional-skyblue
-"#96BA3C" # --additional-green-light
-"#006400" # --additional-green-dark
+he_cols_1 <- c("#0e688b", # brand main
+               "#107eab", # .. light
+               "#005379", # .. active
+               "#003146", # .. dark
+               "#000222"  # .. nearly-black)
+)
+
+he_nams_1 <- paste("UH", 
+                   c("main", 
+                     "light", 
+                     "active", 
+                     "dark", 
+                     "nearly-black"
+                   ))
+
+
+# 2: Grayscale colors ---- 
+# 
+# "#ffffff" #	white
+# "#f8f8f8" # grayscale-light 	
+# "#979797" # grayscale-medium-dark 
+# "#555555" # grayscale-dark
+# "#000000" # black
+
+he_cols_2 <- c("#ffffff", #	white
+               "#f8f8f8", # grayscale-light 	
+               "#979797", # grayscale-medium-dark 
+               "#555555", # grayscale-dark
+               "#000000"  # black
+)
+
+he_nams_2 <- c("white", 
+               "light",  	
+               "medium dark",  
+               "dark", 
+               "black"
+)
+
+
+# 3: Colors reserved for departmental use ---- 
+# 
+# "#E5053A" # --additional-red-light
+# "#A31621" # --additional-red-dark
+# "#420030" # --additional-purple-light
+# "#F9A21A" # --additional-yellow
+# "#48C5F8" # --additional-skyblue
+# "#96BA3C" # --additional-green-light
+# "#006400" # --additional-green-dark
+
+
+he_cols_3 <- c("#E5053A", # --additional-red-light
+               "#A31621", # --additional-red-dark
+               "#420030", # --additional-purple-light
+               "#F9A21A", # --additional-yellow
+               "#48C5F8", # --additional-skyblue
+               "#96BA3C", # --additional-green-light
+               "#006400" # --additional-green-dark
+)
+
+he_nams_3 <- paste("add", 
+                   c("red light", 
+                     "red dark", 
+                     "purple light", 
+                     "yellow", 
+                     "skyblue",
+                     "green light", 
+                     "green dark"
+                   ))
+
 
 
 # Color palette(s):
